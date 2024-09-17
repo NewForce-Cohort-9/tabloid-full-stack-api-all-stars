e311ba5e1a2ea223ad50af502eed122f0e8d2d01
Will Moran <will.c.moran@gmail.com>
2024-09-17 14:35:09 -0400

    Added navbar link to posts

--- TabloidFullStack/TabloidFullStack/client/tabloid/src/components/Header.js
+++ TabloidFullStack/TabloidFullStack/client/tabloid/src/components/Header.js
@@ -39,6 +39,9 @@ export default function Header({isLoggedIn, setIsLoggedIn}) {
                       setIsLoggedIn(false)
                     }}>Logout</a>
                 </NavItem>
+                <NavItem>
+                  <NavLink tag={RRNavLink} to="/posts">Posts</NavLink>
+                </NavItem>
               </>
             }
             {!isLoggedIn &&
