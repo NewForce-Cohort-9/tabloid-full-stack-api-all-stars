e95d10732d76312255f6bb8bbaf0985715b060a8
Jessica Sturgell <sturgell01@gmail.com>
2024-09-17 16:06:35 -0400

    updated to include post title and user profile information

--- TabloidFullStack/TabloidFullStack/client/tabloid/src/components/CommentList.js
+++ TabloidFullStack/TabloidFullStack/client/tabloid/src/components/CommentList.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { getCommentsByPostId } from "../Managers/CommentManager.js";
+import { useNavigate } from "react-router-dom";
 
 export default function CommentList({ postId }) {
   const [comments, setComments] = useState([]);
+  const navigate = useNavigate();
 
   const getComments = () => {
     getCommentsByPostId(postId).then((allComments) => setComments(allComments));
@@ -16,8 +18,22 @@ export default function CommentList({ postId }) {
 
   return (
     <>
+      {/* will temporarily link back to all posts until post details is finished */}
+      <h2
+        onClick={() => {
+          navigate("/posts");
+        }}
+      >
+        {comments[0]?.post.title} comments
+      </h2>
       {comments.map((comment) => (
-        <p key={comment.id}>{comment.subject}</p>
+        <div key={comment.id}>
+          <p>SUBJECT: {comment.subject}</p>
+          <p>CONTENT: {comment.content}</p>
+          <p>AUTHOR: {comment.userProfile?.displayName}</p>
+          <p>DATE: {comment.createDateTime}</p>
+          <hr />
+        </div>
       ))}
     </>
   );
