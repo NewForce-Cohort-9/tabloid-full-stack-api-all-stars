59067df28c624869b7d320cbed85750f2ed1db4b
Meowph <bpardue33@gmail.com>
2024-09-18 14:34:00 -0400

    New Tags Can Be Added

--- TabloidFullStack/TabloidFullStack/client/tabloid/src/components/ApplicationViews.js
+++ TabloidFullStack/TabloidFullStack/client/tabloid/src/components/ApplicationViews.js
@@ -3,10 +3,11 @@ import { Route, Routes } from "react-router-dom";
 import Hello from "./Hello";
 import { PostList } from "./Post/PostList.js";
 import { CategoryList } from "./CategoryList.js";
-import { TagList } from "./TagList.js";
+import { TagList } from "./Tags/TagList.js";
 import { UserPostList } from "./Post/UserPostList.js";
 import { PostDetails } from "./Post/PostDetails.js";
 import { CategoryForm } from "./CategoryForm.js";
+import { TagForm } from "./Tags/TagForm.js";
 
 export default function ApplicationViews({ currentUser }) {
 
@@ -18,8 +19,8 @@ export default function ApplicationViews({ currentUser }) {
         <Route path="/myposts" element={<UserPostList />} />
         <Route path="/post/:id" element={<PostDetails />} />
         <Route path="/newCategory" element={<CategoryForm />} />
-        <Route path="/tags" element={<TagList />} />
-
+        <Route path="/Tags" element={<TagList />} />
+        <Route path="/newTagAdded" element={<TagForm />} />
       </Routes>
    );
  
