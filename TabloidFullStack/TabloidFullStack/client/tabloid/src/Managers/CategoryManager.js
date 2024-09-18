2f983504f63245d4db35b630a7363060d04e4e1c
Iris Lee <irislee4git@gmail.com>
2024-09-18 09:58:31 -0400

    add to category manager

--- TabloidFullStack/TabloidFullStack/client/tabloid/src/Managers/CategoryManager.js
+++ TabloidFullStack/TabloidFullStack/client/tabloid/src/Managers/CategoryManager.js
@@ -4,3 +4,13 @@ export const getAllCategories = () => {
     return fetch(apiUrl)
     .then((res) => res.json())
 };
+
+export const addCategory = (category) => {
+    return fetch(apiUrl, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(category),
+    });
+};
