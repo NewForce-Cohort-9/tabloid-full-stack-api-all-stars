7a68074e3a809bac8994fa0b362ecade3a6c2ca9
Iris Lee <irislee4git@gmail.com>
2024-09-18 14:44:02 -0400

    update controller

--- TabloidFullStack/TabloidFullStack/Controllers/CategoryController.cs
+++ TabloidFullStack/TabloidFullStack/Controllers/CategoryController.cs
@@ -41,8 +41,15 @@ namespace TabloidFullStack.Controllers
 
         // PUT api/<CategoryController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public IActionResult Put(int id, Category category)
         {
+            if (id != category.Id)
+            {
+                return BadRequest();
+            }
+
+            _categoryRepository.Update(category);
+            return NoContent();
         }
 
         // DELETE api/<CategoryController>/5
