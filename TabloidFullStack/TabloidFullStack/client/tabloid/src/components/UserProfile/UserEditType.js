9f1d1508a0ffb45f6e3a8ad053a7e48d7c0683c2
Iris Lee <irislee4git@gmail.com>
2024-09-26 11:28:00 -0400

    function works

--- TabloidFullStack/TabloidFullStack/client/tabloid/src/components/UserProfile/UserEditType.js
+++ TabloidFullStack/TabloidFullStack/client/tabloid/src/components/UserProfile/UserEditType.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 import { useNavigate, useParams } from "react-router-dom"
 import { getUserById, updateUserType } from "../../Managers/UserProfileManager.js";
-import { Button, Container, Form, Input, ListGroupItemHeading } from "reactstrap";
+import { Button, Container, Form, FormGroup, Input, Label, ListGroupItemHeading } from "reactstrap";
 
 export const UserEditType = () => {
     
@@ -45,13 +45,59 @@ export const UserEditType = () => {
                 <ListGroupItemHeading>
                     Edit User Type: {user.displayName}
                 </ListGroupItemHeading>
-                <Input
+                {/* <Input
                 type="number"
                 name="userTypeId"
                 value={user.userTypeId}
                 onChange={handleInputChange}
-                />
-            </Form>
+                /> */}
+
+            <FormGroup>
+                <Label for="userType">
+                    User Type
+                </Label>
+                    {(user.userTypeId === 1 || user.userTypeId === "1" ) && (
+                    <Input
+                        name="userTypeId"
+                        type="select"
+                        onChange={handleInputChange}
+                    >
+                        <option
+                    value="1"
+                    selected
+                    >
+                     Admin    
+                    </option> 
+                    <option
+                    value="2"
+                    >
+                        Author
+                    </option>
+                    </Input>
+                    )}
+
+                {(user.userTypeId === 2 || user.userTypeId === "2") && (
+                    <Input
+                        name="userTypeId"
+                        type="select"
+                        onChange={handleInputChange}
+
+                    >
+                        <option
+                    value="1"
+                    >
+                     Admin    
+                    </option> 
+                    <option
+                    value="2"
+                    selected
+                    >
+                        Author
+                    </option>
+                    </Input>
+                    )}
+                    
+            </FormGroup>
             <Button
             color="warning"
             size="sm"
@@ -66,6 +112,7 @@ export const UserEditType = () => {
             >
                 **Save**
             </Button>
+            </Form>
         </Container>
     )
 }
