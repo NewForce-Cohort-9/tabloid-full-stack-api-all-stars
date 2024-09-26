1464e39cf68ddcabf7e3d4204ffa43dd429d8bfc
Iris Lee <irislee4git@gmail.com>
2024-09-26 10:16:49 -0400

    form loads

--- TabloidFullStack/TabloidFullStack/client/tabloid/src/components/UserProfile/UserEditType.js
+++ TabloidFullStack/TabloidFullStack/client/tabloid/src/components/UserProfile/UserEditType.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 import { useNavigate, useParams } from "react-router-dom"
-import { getUserById } from "../../Managers/UserProfileManager.js";
-import { Container } from "reactstrap";
+import { getUserById, updateUserType } from "../../Managers/UserProfileManager.js";
+import { Button, Container, Form, Input, ListGroupItemHeading } from "reactstrap";
 
 export const UserEditType = () => {
     
@@ -14,9 +14,54 @@ export const UserEditType = () => {
         getUserById(userId).then(userObj => setUser(userObj))
     }, [userId])
 
+    const handleInputChange = (e) => {
+        const copy = { ...user }
+          copy[e.target.name] = e.target.value
+          setUser(copy)
+          }
+  
+  
+      const handleEdit = () => {
+  
+          const editedUser= {
+              id: user.id,
+              userTypeId: user.userTypeId,
+          }
+  
+          updateUserType(editedUser)
+          .then(() => {
+              navigate(`users/${user.id}`)
+          })
+      }
+  
+
     return(
         <Container>
-            
+            <Form>
+                <ListGroupItemHeading>
+                    Edit User
+                </ListGroupItemHeading>
+                <Input
+                type="number"
+                name="userTypeId"
+                value={user.userTypeId}
+                onChange={handleInputChange}
+                />
+            </Form>
+            <Button
+            color="warning"
+            size="sm"
+            onClick={() => {navigate(`/users/${user.id}`)}}
+            >
+                Nevermind!
+            </Button>
+            <Button
+            color="success"
+            size="sm"
+            onClick={handleEdit}
+            >
+                **Save**
+            </Button>
         </Container>
     )
 }
