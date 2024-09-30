528d95b61317d95c25de78ada96d86ed56a03863
Iris Lee <irislee4git@gmail.com>
2024-09-30 13:20:21 -0400

    set up deactivated list

---
+++ TabloidFullStack/TabloidFullStack/client/tabloid/src/components/UserProfile/DeactivatedUserList.js
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react"
+import { getAllUsers } from "../../Managers/UserProfileManager.js";
+import { Button, Container, List, ListGroup, ListGroupItem } from "reactstrap";
+import { useNavigate } from "react-router-dom";
+
+export const UserList = () => {
+
+    const [users, setUsers] = useState([]);
+
+    const getUsers = () => {
+        getAllUsers().then(usersObj => setUsers(usersObj))
+    };
+
+    useEffect(() => {
+        getUsers();
+    }, []);
+
+    const navigate = useNavigate();
+
+    return(
+        <Container>
+            <List>
+                <ListGroup>
+                    All Users
+                </ListGroup>
+                {users.map((user) => (
+                    <ListGroupItem
+                    key={user.id}
+                    >
+                        Display Name: {user.displayName} 
+                        Full Name: {user.fullName}
+                        User Type: {user.userType.name}
+                    <Button
+                    color="success"
+                    onClick={() => {navigate(`/users/editType/${user.id}`)}}
+                    >
+                        Edit
+                    </Button>
+                    <Button
+                    onClick={() => {navigate(`/users/${user.id}`)}}
+                    >
+                        View Details
+                    </Button>
+                    <button
+                    onClick={() => {navigate(`/users/deactivate/${user.id}`)}}
+                    >
+                        Deactivate User
+                    </button>
+                    </ListGroupItem>
+                ))}
+            </List>
+        </Container>
+    )
+}
