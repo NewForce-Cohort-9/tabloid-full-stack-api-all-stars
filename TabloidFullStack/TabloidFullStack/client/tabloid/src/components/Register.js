6c96d5d65c7a2d2cb86daf63f439abd26bdc8bfd
Iris Lee <irislee4git@gmail.com>
2024-10-01 10:13:02 -0400

    photo loads

--- TabloidFullStack/TabloidFullStack/client/tabloid/src/components/Register.js
+++ TabloidFullStack/TabloidFullStack/client/tabloid/src/components/Register.js
@@ -72,8 +72,11 @@ export default function Register({ setIsLoggedIn }) {
           <Label htmlFor="imageLocation">Profile Image URL</Label>
           <Input
             id="imageLocation"
-            type="text"
-            onChange={(e) => setImageLocation(e.target.value)}
+            type="file"
+            onChange={(e) => {
+              console.log(e.target.files[0]);
+              setImageLocation(URL.createObjectURL(e.target.files[0]))
+            }}
           />
         </FormGroup>
         <FormGroup>
